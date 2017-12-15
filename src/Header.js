4185d6772d77a0a1b672aa24a3a136be8134c412
troysipp <sipprelle_troy@bah.com>
2017-12-15 16:43:56 -0500

    add styling to header

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {
-  Link
-} from "react-router-dom"
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Header = ({}) => {
-
+  const headerStyle = {
+    display: "flex",
+    justifyContent: "space-around"
+  };
   return (
     <div>
-      <nav>
+      <nav style={headerStyle}>
         <h1>Home App</h1>
         <Link to="/">Home</Link>
         <Link to="/new-home">New Home</Link>
         <Link to="/login">Login</Link>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-
-export default Header
+export default Header;
