c80cd667ecd03d0e58c8c127ceb7b4307ea55368
troysipp <sipprelle_troy@bah.com>
2017-12-18 14:23:03 -0500

    get frontend ready for homelist data and add onsubmit prop

--- src/HomeList.js
+++ src/HomeList.js
@@ -5,24 +5,19 @@ import HomeSummary from "./HomeSummary";
 class HomeList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
   }
 
   render() {
-    // let homes = this.props.homes.map((home, i) => {
-    //   return (
-    //     <li className="home" key={i}>
-    //       {<HomeSummary home={home} />}
-    //     </li>
-    //   );
-    // });
+    let homes = this.props.homes.map((home, i) => {
+      return (
+        <li className="home" key={i}>
+          {<HomeSummary home={home} />}
+        </li>
+      );
+    });
     return (
       <div>
-        <ul>
-          {
-            //homes
-          }
-        </ul>
+        <ul>{homes}</ul>
       </div>
     );
   }
--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -1,95 +1,93 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import TextInput from './TextInput'
-import PasswordInput from './PasswordInput'
-import axios from "axios"
-
+import React from "react";
+import PropTypes from "prop-types";
+import TextInput from "./TextInput";
+import PasswordInput from "./PasswordInput";
+import axios from "axios";
 
 class SignUpForm extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       user: {
-        email: '',
-        password: '',
-        confirmPassword: ''
+        email: "",
+        password: "",
+        confirmPassword: ""
       },
       errors: {},
-      submitted: false,
-    }
-    this.onChange = this.onChange.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
+      submitted: false
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onChange (event) {
-    const user = this.state.user
-    user[event.target.name] = event.target.value
-    this.setState({ user })
+  onChange(event) {
+    const user = this.state.user;
+    user[event.target.name] = event.target.value;
+    this.setState({ user });
   }
 
-  passwordStrength (password) {
-    if (!password) return null
-    if (password.length >= this.props.minPasswordLength) return 100
-    const percentOfMinLength = parseInt(password.length / this.props.minPasswordLength * 100, 10)
-    return percentOfMinLength
+  passwordStrength(password) {
+    if (!password) return null;
+    if (password.length >= this.props.minPasswordLength) return 100;
+    const percentOfMinLength = parseInt(
+      password.length / this.props.minPasswordLength * 100,
+      10
+    );
+    return percentOfMinLength;
   }
 
-  validate ({ email, password, confirmPassword }) {
-    const errors = {}
-    const { minPasswordLength } = this.props
-    const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  validate({ email, password, confirmPassword }) {
+    const errors = {};
+    const { minPasswordLength } = this.props;
+    const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-    if (!email)
-      errors.email = 'Email required.'
-    if (!emailRegex.test(email))
-      errors.email = 'Email must be valid.'
+    if (!email) errors.email = "Email required.";
+    if (!emailRegex.test(email)) errors.email = "Email must be valid.";
     if (password.length < minPasswordLength)
-      errors.password = `Password must be at least ${minPasswordLength} characters.`
-    if (password !== confirmPassword)
-      errors.password = `Passwords must match`
+      errors.password = `Password must be at least ${minPasswordLength} characters.`;
+    if (password !== confirmPassword) errors.password = `Passwords must match`;
 
-    this.setState({ errors })
-    const formIsValid = Object.getOwnPropertyNames(errors).length === 0
-    return formIsValid
+    this.setState({ errors });
+    const formIsValid = Object.getOwnPropertyNames(errors).length === 0;
+    return formIsValid;
   }
 
-  onSubmit (event) {
-    event.preventDefault()
-    const formIsValid = this.validate(this.state.user)
+  onSubmit(event) {
+    event.preventDefault();
+    const formIsValid = this.validate(this.state.user);
     if (formIsValid) {
-      this.props.onSubmit(this.state.user)
-      this.setState({ submitted: true })
+      this.props.onSubmit(this.state.user);
+      this.setState({ submitted: true });
     }
     axios
       .post("http://localhost:3001/signup", {
         email: this.state.user.email,
         password: this.state.user.password
       })
-      .then((response) => {
-        console.log(response.data.token)
-        localStorage.token = response.data.token
-         console.log(localStorage.token)
-        this.props.history.push("/")
+      .then(response => {
+        console.log(response);
+        localStorage.token = response.data.token;
+        console.log(localStorage.token);
+        this.props.history.push("/");
       })
       .catch(err => {
-        console.log(err)
-      })
+        console.log(err);
+      });
   }
 
   render() {
-    const { errors, submitted } = this.state
-    const { email, password, confirmPassword } = this.state.user
+    const { errors, submitted } = this.state;
+    const { email, password, confirmPassword } = this.state.user;
     const formStyle = {
-      background: 'rgb(222, 222, 222)',
-      border: 'rgb(0, 0, 0)',
-      width: '400px',
-      padding: '1em'
-    }
-    return (
-      submitted
-      ? <h2>{this.props.confirmationMessage}</h2>
-      : <div style={formStyle}>
+      background: "rgb(222, 222, 222)",
+      border: "rgb(0, 0, 0)",
+      width: "400px",
+      padding: "1em"
+    };
+    return submitted ? (
+      <h2>{this.props.confirmationMessage}</h2>
+    ) : (
+      <div style={formStyle}>
         <TextInput
           htmlId="signup-form-email"
           labelName="Email"
@@ -118,7 +116,7 @@ class SignUpForm extends React.Component {
         />
         <input type="submit" value="Sign Up" onClick={this.onSubmit} />
       </div>
-    )
+    );
   }
 }
 
@@ -126,11 +124,12 @@ SignUpForm.propTypes = {
   confirmationMessage: PropTypes.string,
   onSubmit: PropTypes.func.isRequired,
   minPasswordLength: PropTypes.number
-}
+};
 
 SignUpForm.defaultProps = {
-  confirmationMessage: "Thanks for signing up! You should receive an email confirmation shortly.",
+  confirmationMessage:
+    "Thanks for signing up! You should receive an email confirmation shortly.",
   minPasswordLength: 8
-}
+};
 
-export default SignUpForm
+export default SignUpForm;
