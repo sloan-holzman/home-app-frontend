293a6f99407021bb9375b6edcd464959107c4e27
Sloan Holzman <sloan.holzman@gmail.com>
2017-12-18 19:02:08 -0500

    complete basic showpage, though have to do it during map

--- src/HomeShow.js
+++ src/HomeShow.js
@@ -1,54 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
 
 class HomeShow extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      owner: false
-    };
-  }
-
-  checkOwner() {
-    axios
-      .get(`http://localhost:3001/api/homes/${this.props.match.params.id}`, {
-        token: localStorage.token,
-      })
-      .then(response => {
-        this.setOwner(response.data)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
-
-  setOwner(response) {
-    this.setState({
-      owner: response,
-    })
-  }
-
-  componentDidMount() {
-    this.checkOwner()
   }
 
 
   render() {
-    let home = this.props.homes.filter(home => home.id === this.props.match.params.id)[0]
-    let unit = (home.unit ? `, ${home.unit}` : "")
-    let price = (home.type_rent_buy == "Rent" ? `${home.price_range} per month` : `Price: ${home.price_range}`)
+    // let home = this.props.homes.filter((home) => home._id === this.props.match.params.id)
+    // console.log(home[0])
+    // let unit = (home.unit ? `, ${home.unit}` : "")
+    // let price = (home.type_rent_buy === "Rent" ? `${home.price_range} per month` : `Price: ${home.price_range}`)
+    let homes = this.props.homes.map((home, i) => {
+      if (home._id === this.props.match.params.id) {
+        console.log(home)
+        let unit = (home.unit ? `, ${home.unit}` : "")
+        let price = (home.type_rent_buy === "Rent" ? `$${home.price_range} per month` : `Price: ${home.price_range}`)
+        return (
+          <div key={i}>
+            <img src={home.img_url} alt={home.street_address}/>
+            <h3>{home.street_address} {unit}</h3>
+            <p>{home.city}, {home.state} {home.zipcode}</p>
+            <p>{price}</p>
+            <p>{home.num_bed} beds - {home.num_bath} baths - {home.sq_ft} sqft</p>
+          </div>
+        );
+      }
+    });
+
     return (
       <div>
-        <img src={home.img_url} alt="Picture of home"/>
-        <h3>{home.street_address} {unit}</h3>
-        <h3>{home.city}, {home.state} {home.zipcode}</h3>
-        <h4>{home.num_bed} bed -  {home.num_bath} bath -  {home.sq_ft} sqft</h4>
-        <h4>{price}</h4>
-        {
-          {this.state.owner} ?
-            <p><a href="/api/homes/"+{this.props.match.params.id}>Edit Home</a></p> :
-            <p></p>
-        }
+        {homes}
       </div>
     )
   }
